eb744d485fc219b0a3771e3bb0d864b8b2eb934c
evgenia-tkachenko <icedancingby@gmail.com>
2018-02-28 20:42:43 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import GoodsPage from './GoodsPage';
+import CategoriesPage from './CategoriesPage';
+const ReactRouterDom = require("react-router-dom");
+const Router = ReactRouterDom.BrowserRouter;
+const Route = ReactRouterDom.Route;
+const Link = ReactRouterDom.Link;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Here is my App
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePage: "goods"
+        }
+    }
+
+    changeView(e) {
+        
+    }
+
+    render() {
+        return (
+            <Router>
+                <Fragment>
+
+                    <div className="title">
+                        Каталог товаров
+                    </div>
+
+                    <nav>
+                        <div className="menu">
+                            <Link to="/">
+                                <div className="link">
+                                    Товары
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="menu">
+                            <Link to="/categories">
+                                <div className="link">
+                                    Категории
+                                </div>
+                            </Link>
+                        </div>
+                    </nav>
+
+                    <Route exact path="/" component={GoodsPage} />
+                    <Route path="/categories" component={CategoriesPage} />
+
+                    <footer>
+                        <hr/>
+                    </footer>
+
+                </Fragment>
+            </Router>
+        );
+    }
 }
 
 export default App;
